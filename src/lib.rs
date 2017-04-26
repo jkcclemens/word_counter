9a66c5eceb3a665cb4b75fbaf88cfaa3719fca40
Kyle Clemens <me@kyleclemens.com>
2017-04-26 19:31:02 -0400

    fix: split whitespace

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 
 pub fn count_words(input: &str) -> HashMap<String, usize> {
   let mut map = HashMap::new();
-  for word in input.split(' ') {
+  for word in input.split_whitespace() {
     let word: String = word.chars().filter(|c| c.is_alphanumeric()).collect();
     *map.entry(word.to_lowercase()).or_insert(0) += 1;
   }
